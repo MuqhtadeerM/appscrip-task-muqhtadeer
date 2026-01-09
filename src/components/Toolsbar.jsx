b048705e8f6028c5b245ff9aff38d450b70d4ce8
MuqhtadeerM <muhammedmuzawar9@gmail.com>
2026-01-09 21:06:29 +0530

    feat: added tool bar

--- src/components/Toolsbar.jsx
+++ src/components/Toolsbar.jsx
@@ -2,11 +2,51 @@ import { useState } from "react";
 
 export default function Toolbar() {
   const [showFilter, setShowFilter] = useState(true);
+  const [showSort, setSortFilter] = useState(false);
+  const [selectedSort, setSelectedSort] = useState("RECOMMENDED");
+
+  const sortOptions = [
+    "RECOMMENDED",
+    "NEWEST FIRST",
+    "POPULAR",
+    "PRICE: HIGH TO LOW",
+    "PRICE: LOW TO HIGH",
+  ];
 
   return (
     <section className="toolbar">
       <div className="toolbarLeft">
         <span className="itemCount">3425 Items</span>
+
+        <button
+          className="filterToggle"
+          onClick={() => setShowFilter(!showFilter)}
+        >
+          {showFilter ? "HIDE FILTER" : "SHOW FILTER"}
+        </button>
+      </div>
+
+      <div className="toolbarRight">
+        <button className="sortButton" onClick={() => setShortSort(!showSort)}>
+          {selectedSort}
+        </button>
+
+        {showSort && (
+          <ul className="sortDropdown">
+            {sortOptions.map((option) => (
+              <li
+                key={option}
+                className={option === selectedSort ? "active" : ""}
+                onClick={() => {
+                  setSelectedSort(option);
+                  setShowFilter(false);
+                }}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </section>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Toolbar from "../components/Toolsbar";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Toolbar />
       <main>
         <h1>Meta Data</h1>
       </main>
